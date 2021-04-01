2362c2b989d5241b86e3e0c3191252ba5ccf7785
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-04-02 00:54:07 +0300

    feat: add menu burger

--- js/main.js
+++ js/main.js
@@ -7,4 +7,16 @@
             header.classList.remove('header_active')
         }
     }
+
+    const nav = document.querySelector(".header__nav");
+
+    const burger = document.querySelector(".header__burger");
+    burger.addEventListener("click", () => {
+      nav.classList.add("header__nav_active");
+    });
+
+    const close = document.querySelector(".header__nav-close");
+    close.addEventListener("click", () => {
+      nav.classList.remove("header__nav_active");
+    });
 }());
